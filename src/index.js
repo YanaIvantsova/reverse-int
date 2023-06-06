f7e452da20e2440a24fecfe46c65ad2d3af7e99d
yanaivantsova <yanaivantsova@yandex.by>
2023-06-06 16:17:23 +0200

    add task decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let num = Math.abs(n);
+    console.log(num);
+    let arr = num.toString(10).split('').map(int => parseInt(int, 10));
+    console.log(arr);
+    arr.reverse();
+
+    return  Number(arr.join(''));
 }
